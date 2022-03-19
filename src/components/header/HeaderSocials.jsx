8300805810ba4c0dd614d233e69b2a42287b99a8
tony <nguyenhuytoan1994@outlook.com>
2022-03-19 16:16:40 +0700

    Add header section

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FiDribbble } from "react-icons/fi";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="__blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="__blank">
+        <FaGithub />
+      </a>
+      <a href="https://dribbble.com" target="__blank">
+        <FiDribbble />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
