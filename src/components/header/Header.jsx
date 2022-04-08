2638a3362a41de9a64f2003e7a6f6e3109cf6258
tony <nguyenhuytoan1994@outlook.com>
2022-04-08 11:20:56 +0700

    Update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello, I'm</h5>
-        <h1>Tony Nguyen 2</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h1>Tony Nguyen</h1>
+        <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <HeaderSocials />
 
